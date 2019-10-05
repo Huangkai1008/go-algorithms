a92a4aff98b03ac7a9f2acda5d7fcad732a3a399
huangkai <h1770360848@outlook.com>
2019-10-05 22:25:51 +0800

    docs(array): 补充注释

--- data_strutures/array/dynamic_array.go
+++ data_strutures/array/dynamic_array.go
@@ -1,29 +1,28 @@
 package array
 
+// Array 是数组的抽象类型
 type Array interface {
-	Get(index int) interface{}
+	Get(index int) interface{}          // Get 返回数组索引位置元素的值
+	Append(item interface{})            // Append 添加元素到数组尾部
+	Set(index int, item interface{})    // Set 设置数组索引位置元素的值
+	Insert(index int, item interface{}) // Insert 插入元素到数组的索引位置
+	Remove(index int) interface{}       // Remove 删除数组索引位置的元素，并返回此元素的值
+	Contains(item interface{}) bool     // Contains 判断元素是否在数组中
+	Search(item interface{}) int        // Search 返回元素第一次出现的索引位置，如果元素不存在，返回-1
 
-	Append(item interface{})
-
-	Set(index int, item interface{})
-	Insert(index int, item interface{})
-
-	Remove(index int) interface{}
-
-	Contains(item interface{}) bool
-	Search(item interface{}) int
-
-	IsEmpty() bool
-	Size() int
-	Capacity() int
+	IsEmpty() bool // IsEmpty 判断数组是否为空
+	Size() int     // Size 返回数组中元素的数量
+	Capacity() int // Capacity 返回数组的容量
 }
 
+// DynamicArray 实现动态数组
 type DynamicArray struct {
 	capacity int
 	size     int
 	items    []interface{}
 }
 
+// New 返回一个空动态数组
 func New(capacity int) *DynamicArray {
 	return &DynamicArray{
 		capacity: capacity,
@@ -62,6 +61,7 @@ func (a *DynamicArray) Insert(index int, item interface{}) {
 		panic("Insert item failed, index out of range.")
 	}
 
+	// 当数组的元素数量等于数组容量时需要进行扩容
 	if a.size == a.capacity {
 		a.resize(a.capacity * 2)
 	}
@@ -85,6 +85,8 @@ func (a *DynamicArray) Remove(index int) interface{} {
 	a.items[a.size-1] = nil
 	a.size--
 
+	// 当数组的元素数量等于数组容量的1/4时需要进行缩容
+	// 同时需要考虑边界情况，缩容后的容量不能为0
 	if a.size == a.capacity/4 && a.capacity/2 != 0 {
 		a.resize(a.capacity / 2)
 	}
@@ -122,6 +124,7 @@ func (a *DynamicArray) Capacity() int {
 	return a.capacity
 }
 
+// resize 进行数组的扩缩容
 func (a *DynamicArray) resize(capacity int) {
 	newItems := make([]interface{}, capacity)
 	for i := 0; i < a.size; i++ {
