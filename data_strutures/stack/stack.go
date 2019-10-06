d9456572e121c5d6f05503b4551d25711dba2809
huangkai <h1770360848@outlook.com>
2019-10-06 21:13:48 +0800

    feat(stack): 实现栈的接口

---
+++ data_strutures/stack/stack.go
@@ -0,0 +1,13 @@
+package stack
+
+// Stack 是栈的抽象类型
+// 栈是一种后进先出（FIFO）的数据结构
+//
+// Ref: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+type Stack interface {
+	Push(data interface{}) // Push 添加元素到栈顶
+	Pop() interface{}      // Pop 弹出栈顶元素
+	Peek() interface{}     // Peek 查看栈顶元素
+	IsEmpty() bool         // IsEmpty 判断栈是否为空
+	Size() int             // Size 返回栈内元素的数量
+}
