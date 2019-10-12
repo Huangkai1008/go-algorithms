e0ec9a3bfd48f2e86d08b16b5cc710df530d8310
huangkai <h1770360848@outlook.com>
2019-10-12 22:11:14 +0800

    test: bdd重写背包测试

--- data_structures/bag/bag_test.go
+++ data_structures/bag/bag_test.go
@@ -3,17 +3,46 @@ package bag
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func TestBag(t *testing.T) {
-	bag := New()
-	assert.Equal(t, true, bag.IsEmpty(), "The bag should be empty.")
-	assert.Equal(t, 0, bag.Size(), "The empty bag's size should be zero.")
+var _ = Describe("Bag", func() {
+	var bag *Bag
 
-	bag.Add(1)
-	bag.Add(2)
-	bag.Add(3)
-	assert.Equal(t, 3, bag.Size(), "The bag's size should be three now.")
-	assert.Equal(t, false, bag.IsEmpty(), "The bag should be not empty.")
+	BeforeEach(func() {
+		By("Create a new bag")
+		bag = New()
+	})
+
+	Describe("This is a new bag", func() {
+		It("should be empty", func() {
+			Expect(bag.IsEmpty()).To(BeTrue())
+		})
+
+		Specify("The empty bag's size should be zero", func() {
+			Expect(bag.Size()).To(BeZero())
+		})
+	})
+
+	Describe("The bag can add items", func() {
+		JustBeforeEach(func() {
+			By("Add three items to the bag")
+			bag.Add(1)
+			bag.Add(2)
+			bag.Add(3)
+		})
+
+		It("should not be empty", func() {
+			Expect(bag.IsEmpty()).To(BeFalse())
+		})
+		Specify("The bag's size should be three now", func() {
+			Expect(bag.Size()).To(Equal(3))
+		})
+	})
+})
+
+func TestBooks(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Bag Suite")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go-algorithms
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/onsi/ginkgo/v2 v2.0.0 // indirect
+	github.com/onsi/gomega v1.17.0 // indirect
+	github.com/stretchr/testify v1.7.0
+)
